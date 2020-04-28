4506303654c03cfc171fbba27721271911cc5979
rgoluch <rgoluch@iastate.edu>
2020-04-28 18:44:07 -0500

    first commit

---
+++ FoodTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by Ryan Goluch on 4/28/20.
+//  Copyright © 2020 Ryan Goluch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
